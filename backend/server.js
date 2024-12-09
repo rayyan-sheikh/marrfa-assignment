a19a84071fa8f60fc7cc15f4cc404f924d290a5f
Rayyan <113837424+rayyan-sheikh@users.noreply.github.com>
2024-12-09 13:20:25 +0530

    More Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ connectDB()
 
 const app = express();
 
-app.use(cors());
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+app.use(cors({
+  origin: allowedOrigin
+}));
 app.use(express.json())
 
 app.use('/blogs', blogRoutes);
